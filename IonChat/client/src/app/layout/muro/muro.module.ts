ec7fceb5d261096c160da4d761f5e1a57378d791
FranciscoBorja <36020423+FranciscoBorja@users.noreply.github.com>
2019-02-01 10:07:19 -0500

    prototipo de chat

--- IonChat/client/src/app/layout/muro/muro.module.ts
+++ IonChat/client/src/app/layout/muro/muro.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { MuroRoutingModule } from './muro-routing.module';
 import { FormsModule } from '@angular/forms';
 import { MuroComponent } from './muro.component';
+import { FriendsOnComponent } from '../../components/friends-on/friends-on.component';
+import { ChatComponent } from '../chat/chat.component';
 
 @NgModule({
   imports: [CommonModule, MuroRoutingModule, FormsModule],
-  declarations: [MuroComponent]
+  declarations: [MuroComponent, FriendsOnComponent, ChatComponent]
 })
 export class MuroModule {}
